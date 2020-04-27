607954713ddcd29566d2978832cb659531e674b2
Glitch (hello-webpage) <none>
2020-04-27 23:21:13 +0000

    🏩🌧 Checkpoint ./style.css:12897968/449 ./take-action.html:12897968/32 ./script.js:12897968/3429

---
+++ script.js
@@ -0,0 +1,107 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "deleteButton";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("deleteButton");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+//mark checked items - function expression
+var fullItemList = document.getElementById("my-list");
+fullItemList.addEventListener(
+  "click",
+  function(event) {
+    //this is a function expression - it allows us to create a function inside the event listener
+    if (event.target.tagName == "LI") {
+      //find out which element triggered a specified event
+      event.target.classList.toggle("all-done");
+    }
+  },
+  false
+);
+
+/*
+classlist.toggle()
+Toggles between a class name for an element.
+
+The first parameter removes the specified class from an element, and returns false.
+If the class does not exist, it is added to the element, and the return value is true.
+
+The optional second parameter is a Boolean value that forces the class to be added or removed, regardless of whether or not it already existed. For example:
+
+Remove a class: element.classList.toggle("classToRemove", false);
+Add a class: element.classList.toggle("classToAdd", true);
+
+*/
+
+//UNCOMMENT TO ADD, COMPLETE, AND DELETE NEW ITEMS
+/*Add new items
+var addButton = document.getElementById("add-button");
+addButton.addEventListener("click", newActionItem);
+
+function newActionItem() {
+  //Create a new "li" element to hold the action item you enter
+  var newListElement = document.createElement("li");
+
+  //Create a variable to select and store the value of what is entered into the input box
+  //WHAT IS .VALUE?!! Debug alert
+  var inputBoxValue = document.getElementById("task-entry-box").value;
+
+  //Add a variable that creates a new text node containing the value of the input box
+  var actionItemText = document.createTextNode(inputBoxValue);
+
+  //Append or attach the new list text you entered to the new "li" element you created
+  newListElement.appendChild(actionItemText);
+
+  if (inputBoxValue === " ") {
+    alert("Please enter your action item");
+  } else {
+    //Find and select the HTML ul element called my-list and attach the li
+    //element containing our action item text to the end of the list
+    document.getElementById("my-list").appendChild(newListElement);
+  }
+  //Pretty sure this will remove the text from the input box
+  document.getElementById("task-entry-box").value = "";
+
+  
+  //add delete button to each new item - still need to commemnt this
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "deleteButton";
+  span.appendChild(txt);
+  newListElement.appendChild(span);
+  
+  //ability to delete all new items
+  for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+*/
+
+
+
+
+
