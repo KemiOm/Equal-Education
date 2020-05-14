14f93e77adf9f09535a63ee6aa3a5bd4f5d0ed8c
Glitch (hello-webpage) <none>
2020-05-14 22:28:19 +0000

    ⛑🚊 Checkpoint ./script.js:12897968/911 ./take-action.css:12897968/55 ./about.css:12897968/184

--- script.js
+++ script.js
@@ -5,16 +5,7 @@
 
 /* GOAL: Click on a list item to mark it checked */
 
-var fullItemList = document.getElementById("my-list");
-fullItemList.addEventListener("click", checkOffItem, false);
 
-function checkOffItem(clicked) {
-  //find out which element triggered a specified event
-  if (clicked.target.tagName == "LI") {
-    //apply the CSS rule set outlined in .all-done if condition is met
-    clicked.target.classList.toggle("all-done");
-  }
-}
 
 /************************************************/
 /****************** TRY A LOOP ******************/
@@ -30,16 +21,7 @@ function checkOffItem(clicked) {
 
 /* GOAL: Create a delete button [x] and append (or attach) it to each list item */
 
-var actionListItem = document.getElementsByClassName("action-li");
 
-for (var i = 0; i < 3; i++) {
-  var newSpan = document.createElement("span");
-  var x = document.createTextNode("X");
-  newSpan.appendChild(x);
-  actionListItem[i].appendChild(newSpan);
-  
-  newSpan.className = "deleteButton";
-}
 
 /************************************************/
 /*************** ADD INTERACTION ****************/
@@ -47,15 +29,6 @@ for (var i = 0; i < 3; i++) {
 
 /* GOAL: Click on a close button to hide the current list item */
 
-var deleteBtn = document.getElementsByClassName("deleteButton");
-
-for (var i = 0; i < deleteBtn.length; i++) {
-  deleteBtn[i].addEventListener("click", closeListItem);
-}
-
-function closeListItem() {
-  this.parentElement.style.display = "none";
-}
 
 
 
