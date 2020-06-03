7a84c81fb3361c880e8c06ae763347314e5d0f6a
Glitch (hello-webpage) <none>
2020-06-03 18:28:37 +0000

    💖💿 Checkpoint ./style.css:14032647/619 ./about.html:14032647/17 ./script.js:14032647/38 ./take-action.html:14032647/80

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /************************************************/
 /********* ADD CHECKLIST FUNCTIONALITY **********/
 /************************************************/
@@ -14,16 +13,27 @@
 /* Add code to practice loops here */
 /* Be sure to delete them or comment them out when you are finished with that section */
 
-// #TODO 1: Create an array that contains 5 items (at least one string) using literal notation. 
+
+
+//TODO: Create an array that contains 5 items (at least one string) using literal notation. 
 // Print it to the console to check your work.
 
-// #TODO 2: Use Array.from to create an array from a string in the previous array.
+
+
+
+//TODO: Use Array.from to create an array from a string in the previous array.
 // Print it to the console to check your work.
 
-// #TODO 3: Print the second item from your first array and the last item from your second array to the console. 
 
-// #TODO 4: Create a new array containing 5 of your favorite things. Use the .forEach method to print each of them 
-// out to the console with the phrase “ is my favorite!” after the item. For example, “Chocolate is my favorite!”
+
+
+//TODO: Print the second item from your first array and the last item from your second array to the console. 
+
+
+
+
+//TODO: Create a new array containing 5 of your favorite things. Use the .forEach method to print each of them 
+// Print out to the console with the phrase “ is my favorite!” after the item. For example, “Chocolate is my favorite!”
 
 
 
@@ -32,6 +42,8 @@
 /************************************************/
 
 /* TODO: Create an array from the "action-li" list items and use .forEach to loop through each item and append a delete button element */
+
+
 
 
 
