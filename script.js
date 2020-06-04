6cd2edf86ea4fdf71b009ec3193e04f0b736c14f
Glitch (hello-webpage) <none>
2020-06-04 21:08:42 +0000

    ☄️🍓 Checkpoint ./script.js:12897968/1948 ./take-action.css:12897968/1490

--- script.js
+++ script.js
@@ -2,55 +2,12 @@
 /********* ADD CHECKLIST FUNCTIONALITY **********/
 /************************************************/
 
-/* TODO: Add event listeners that mark an item as checked when a person clicks on it */
+/* TODO: Add a variable to store the "my-list" element */
 
 
+/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 
 
-/************************************************/
-/************* CREATE DELETE BUTTON *************/
-/************************************************/
-
-/* TODO: Create an array from the "action-li" list items and use .forEach to loop through each item and append a delete button element */
+/* TODO: Declare the function checkOffList and add actions inside the { } */
 
 
-
-
-
-/************************************************/
-/*************** ADD INTERACTION ****************/
-/************************************************/
-
-/* TODO: Loop through the delete buttons and add an event listener to each one. Tell the listener to wait for a mouse click, then hide the list item */
-
-
-
-
-/************************************************/
-/*************** PRACTICE ARRAYS ****************/
-/************************************************/
-
-/* Add code to practice loops here */
-/* Be sure to delete them or comment them out when you are finished with that section */
-
-
-
-//TODO: Create an array that contains 5 items (at least one string) using literal notation. 
-// Print it to the console to check your work.
-
-
-
-
-//TODO: Use Array.from to create an array from a string in the previous array.
-// Print it to the console to check your work.
-
-
-
-
-//TODO: Print the second item from your first array and the last item from your second array to the console. 
-
-
-
-
-//TODO: Create a new array containing 5 of your favorite things. Use the .forEach method to print each of them 
-// Print out to the console with the phrase “ is my favorite!” after the item. For example, “Chocolate is my favorite!”
