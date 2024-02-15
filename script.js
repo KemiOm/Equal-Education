5b09908a3a9bfb9260ba26e796ef8d75ae127f01
Kemi Omoniyi <69874313+KemiOm@users.noreply.github.com>
2024-02-15 15:26:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 /********* ADD CHECKLIST FUNCTIONALITY **********/
 /************************************************/
 
-/* TODO: Add a variable to store the "my-list" element */
+
 var myList="my-list";
 var fullItemList = document.getElementById(myList);
 
 
-/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
+
 if (fullItemList) {
   fullItemList.addEventListener("click", checkOffItem, false );
 }
@@ -17,5 +17,4 @@ clicked.target.classList.toggle("all-done");
     }
 }
 
-/* TODO: Declare the function checkOffList and add actions inside the { } */
 
